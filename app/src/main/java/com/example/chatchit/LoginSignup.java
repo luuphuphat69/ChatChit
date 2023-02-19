d01d7d30efd8198e71b049d0048ff3311d47c8f2
Lưu Phú Phát <luuphuphat69@gmail.com>
2023-02-19 20:42:04 +0700

    comit changes

--- app/src/main/java/com/example/chatchit/LoginSignup.java
+++ app/src/main/java/com/example/chatchit/LoginSignup.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,33 +17,43 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginSignup extends AppCompatActivity {
 
-    TextInputLayout email, password;
+    TextInputLayout username, email, password;
     Button signup, login;
     FirebaseAuth auth;
+    DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
 
+        username = findViewById(R.id.username);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         signup = findViewById(R.id.signup);
         login = findViewById(R.id.login);
-
         auth = FirebaseAuth.getInstance();
-
+        db = FirebaseDatabase.getInstance("https://chatchit-81b07-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+        /*
+        * Sự kiện đăng ký
+         */
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String Email = email.getEditText().getText().toString();
                 String Password = password.getEditText().getText().toString();
+                String UserName = username.getEditText().getText().toString();
+
                 auth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            db.child("User").push().setValue(new User(auth.getUid(), UserName, Email, Password));
                             Toast.makeText(LoginSignup.this, "Tạo tài khoản thành công", Toast.LENGTH_LONG).show();
                         }else{
                             Toast.makeText(LoginSignup.this, "Tạo tài khoản không thành công", Toast.LENGTH_LONG).show();
@@ -49,7 +62,9 @@ public class LoginSignup extends AppCompatActivity {
                 });
             }
         });
-
+        /*
+        * Sự kiện đăng nhập
+        * */
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public  void  onClick(View v){
@@ -67,5 +82,15 @@ public class LoginSignup extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = auth.getCurrentUser();
+        if(user != null){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/chatchit/MainActivity.java
+++ app/src/main/java/com/example/chatchit/MainActivity.java
@@ -1,14 +1,129 @@
 package com.example.chatchit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
+    ArrayList<Message> Messages;
+    FirebaseAuth ath;
+    FirebaseUser user;
+    DatabaseReference db;
+    TextInputLayout inputMessage;
+    FloatingActionButton send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        send = findViewById(R.id.send_icon);
+        inputMessage = findViewById(R.id.inputMessage);
+        recyclerView = findViewById(R.id.recyclerview);
+        Messages = new ArrayList<Message>();
+        db = FirebaseDatabase.getInstance("https://chatchit-81b07-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+        ath = FirebaseAuth.getInstance();
+        user = ath.getCurrentUser();
+        String uId = user.getUid();
+        String uEmail = user.getEmail();
+        String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(Calendar.getInstance().getTime());
+
+        /*
+        * Sự kiện gửi tin nhắn
+        * */
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = inputMessage.getEditText().getText().toString();
+                String datetime = "18/2/2023 9:00";
+                /* Tạo path có tên Messages trên Firebase
+                *  Lưu trữ đối tượng Message
+                * */
+                db.child("Messages").push().setValue(new Message(uEmail, msg, timeStamp)).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        inputMessage.getEditText().setText("");
+                    }
+                });
+            }
+        });
+        adapter = new RecyclerViewAdapter(this, Messages);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        receiveMessages();
+    }
+    /*
+    * Lấy dữ liệu từ máy chủ và cập nhật các mục nhận được khi người dùng
+    * cuộn danh sách
+    * */
+    private void receiveMessages(){
+        db.child("Messages").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Messages.clear();
+                for(DataSnapshot snap:snapshot.getChildren()){
+                    Message message = snap.getValue(Message.class);
+                    Messages.add(message);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    //Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.btn_logout:
+                ath.signOut();
+                startActivity(new Intent(this, LoginSignup.class));
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
