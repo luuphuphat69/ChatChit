c0edb498a0d11c98091553a10af08fa7428dfd55
Nhathieu695 <thieunhathieu511@gmail.com>
2023-03-25 16:46:49 +0700

    Update SettingPreferenceFragment.java

--- app/src/main/java/com/example/chatchit/fragment/setting/SettingPreferenceFragment.java
+++ app/src/main/java/com/example/chatchit/fragment/setting/SettingPreferenceFragment.java
@@ -1,31 +1,62 @@
 package com.example.chatchit.fragment.setting;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.chatchit.R;
 
 public class SettingPreferenceFragment extends PreferenceFragmentCompat{
+    Switch switcher;
+    boolean nightMODE;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     @Override
     public void onCreatePreferences( @Nullable Bundle savedInstanceState, @Nullable String rootKey ) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
         Preference backUp = getPreferenceManager().findPreference("backup");
         Preference restore = getPreferenceManager().findPreference("restore");
+        Preference switcher = getPreferenceManager().findPreference("switch");
+
+        sharedPreferences = getContext().getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMODE = sharedPreferences.getBoolean("night", false);
+
         backUp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
-            public boolean onPreferenceClick( @NonNull Preference preference ) {
-                    return false;
+            public boolean onPreferenceClick(@NonNull Preference preference) {
+                return false;
             }
         });
         restore.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
-            public boolean onPreferenceClick( @NonNull Preference preference ) {
+            public boolean onPreferenceClick(@NonNull Preference preference) {
+                return false;
+            }
+        });
+
+        switcher.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(@NonNull Preference preference) {
+                if (nightMODE){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night",false);
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", true);
+                }
+                editor.apply();
                 return false;
             }
         });
